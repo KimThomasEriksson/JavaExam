61815b7c801630ab7786c970aa9f7193660a15e8
Simonn89 <simon.marchesan@gmail.com>
2017-12-27 14:00:31 +0100

    Student and Admin.
    Not sure if completeCourse is correct.

---
+++ src/Server/model/Admin.java
@@ -0,0 +1,14 @@
+package Server.model;
+
+import java.util.ArrayList;
+
+public class Admin extends User {
+    private ArrayList<String> listOfAdmins;
+
+    public Admin(String firstName, String lastName, String email, String password, int birthyear,ArrayList<String> listOfAdmins) {
+        super(firstName, lastName, email, password, birthyear);
+        this.listOfAdmins = new ArrayList<>();
+
+
+    }
+}
---
+++ src/Server/model/Student.java
@@ -0,0 +1,56 @@
+package Server.model;
+
+import java.util.ArrayList;
+
+public class Student extends User {
+
+    private int totalPoints;
+    private ArrayList<String> currentCourses;
+    private ArrayList<String> completedCourses;
+    private ArrayList<String> failedCourses;
+
+
+    public Student(String firstName, String lastName, String email, String password, int birthyear,int totalPoints,ArrayList<String>currentCourses,ArrayList<String>completedCourses,ArrayList<String>failedCourses) {
+        super(firstName,lastName,email,password,birthyear);
+        this.totalPoints = totalPoints;
+        this.currentCourses = new ArrayList<>();
+        this.completedCourses = new ArrayList<>();
+        this.failedCourses = new ArrayList<>();
+
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+//Adds a course to the list if it's not already in the list of courses.
+    public void addCourse(String Course) {
+        for (int i = 0; i <this.currentCourses.size(); i++) {
+            if(this.currentCourses.get(i).equals(Course)){
+                return;
+            }
+        }
+        this.currentCourses.add(Course);
+    }
+
+    public void completeCourse(String Course,boolean completed){
+        for (int i = 0; i <this.currentCourses.size(); i++) {
+            if(this.currentCourses.get(i).equals(Course)) {
+                if(completed) {
+                    this.completedCourses.add(Course);
+                    return;
+                }
+                else {
+                    this.failedCourses.add(Course);
+                    return;
+                }
+            }
+        }
+        this.currentCourses.remove(Course);
+
+    }
+
+}
