f4d002ac17c5a098533441974e58562be53fa1bd
Jens <hjerpe82@gmail.com>
2017-12-28 09:18:00 +0100

    Created Course and Curriculum

--- src/Server/Model/Course.java
+++ src/Server/Model/Course.java
@@ -1,4 +1,39 @@
 package Model;
 
-public class Course {
+public class Course{
+    private int numberOfCourses = 0;
+    private int courseID;
+    private String name;
+    private String subject;
+    private int points;
+    private int numberOfStudents;
+
+    public Course(String name, String subject, int points, int numberOfStudents){
+        this.name = name;
+        this.subject = subject;
+        this.points = points;
+        this.numberOfStudents = numberOfStudents;
+        this.courseID = numberOfCourses;
+        this.numberOfCourses++;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
 }
---
+++ src/Server/Model/Curriculum.java
@@ -0,0 +1,73 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Curriculum{
+    private static int numberOfCurricilum = 0;
+    private int curricilumID;
+    private Course course;
+    private Teacher teacher;
+    private ArrayList<Student> students;
+
+    public Curriculum(Course course, Teacher teacher, ArrayList<Student> student){
+        this.course = course;
+        this.teacher = teacher;
+        this.students = student;
+        this.curricilumID = this.numberOfCurricilum;
+        this.numberOfCurricilum++;
+    }
+
+    public Curriculum(Course course, Teacher teacher){
+        this.course = course;
+        this.teacher = teacher;
+        students = new ArrayList<>();
+        this.curricilumID = this.numberOfCurricilum;
+        this.numberOfCurricilum++;
+
+    }
+
+    public Curriculum(Course course){
+        this.course = course;
+        students = new ArrayList<>();
+        this.curricilumID = this.numberOfCurricilum;
+        this.numberOfCurricilum++;
+
+    }
+
+    public boolean addTeacher(Teacher newTeacher){
+        if(teacher != null){
+            this.teacher = newTeacher;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean removeTeacher(Teacher teacherToRemove){
+        if(teacher != null && teacher.getEmployeeid() == teacherToRemove.getEmployeeid()){
+            this.teacher = null;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean addStudent(Student studentToAdd){
+        if(!(students.size()>this.course.getNumberOfStudents())){
+            students.add(studentToAdd);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void passStudent(Student studentToPass){
+        studentToPass.completeCourse(this.course, true);
+    }
+
+    public void failStudent(Student studentToPass){
+        studentToPass.completeCourse(this.course, false);
+    }
+
+}
